0938db5bdb2234429b1fa1bf5cc978498bd911f3
xiaoxiao <jixiao35@qq.com>
2019-11-19 20:01:18 +0800

    替换跳转连接

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -14,8 +14,30 @@
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav navbar-right">
         <!-- Authentication Links -->
+        @guest
         <li class="nav-item"><a class="nav-link" href="{{  route('login') }}">登录</a></li>
         <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
+        @else
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+              <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
+                    <img src="https://s1.ax1x.com/2019/11/19/M25EMn.jpg" alt="M25EMn.jpg" border="0" class="img-responsive img-circle" width="30px" height="30px"/>
+              </span>
+              {{ Auth::user()->name }}<span class="caret"></span>
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown" role="menu">
+              <a class="dropdown-item" href="">个人中心</a>
+              <a class="dropdown-item" href="">编辑资料</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" id="logout" href="#">
+                <form action="{{ route('logout') }}" method="POST">
+                  {{ csrf_field() }}
+                  <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                </form>
+              </a>
+            </div>
+          </li>
+        @endguest
       </ul>
     </div>
   </div>

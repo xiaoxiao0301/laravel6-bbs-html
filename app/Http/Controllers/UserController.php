328d5f711f2de0f19ba6ace8d74d1ac96804f0a4
xiaoxiao <jixiao35@qq.com>
2019-11-20 19:51:17 +0800

    授权访问

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,24 +5,49 @@ namespace App\Http\Controllers;
 use App\Handlers\ImageUploadHandler;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
+use foo\bar;
+use Gate;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
 
+
     public function edit(User $user)
     {
+//        $this->authorize('update', $user);
+        $response = Gate::inspect('update', $user);
+        if (!$response->allowed()) {
+            session()->flash('danger', '权限不足！');
+            return back();
+        }
+
         return view('users.edit', compact('user'));
     }
 
+    /**
+     * @param UserRequest $request
+     * @param ImageUploadHandler $uploadHandler
+     * @param User $user
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
     public function update(UserRequest $request, ImageUploadHandler $uploadHandler, User $user)
     {
+        $this->authorize('update', $user);
+
         $data = $request->all();
+
         if ($request->avatar) {
-            $result = $uploadHandler->save($request->avatar, 'avatars', $user->id, false);
+            $result = $uploadHandler->save($request->avatar, 'avatars', $user->id, 416);
             if ($result) {
                 $data['avatar'] = $result['path'];
             }

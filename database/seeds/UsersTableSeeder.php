44c89db37a819c82b75b94cd988d7d71ed6e5f54
xiaoxiao <jixiao35@qq.com>
2019-11-21 11:34:49 +0800

    填充用户和话题数据

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 获取 Faker 实例
+        $faker = app(Faker\Generator::class);
+
+        // 头像假数据
+        $avatars = [
+            'https://imgchr.com/i/M5K3pd',
+            'https://imgchr.com/i/M5Kn0K',
+            'https://imgchr.com/i/M5KZOx',
+            'https://imgchr.com/i/M5KMkD'
+        ];
+
+        // 生成数据集合
+        $users = factory(User::class)
+            ->times(10)
+            ->make()
+            ->each(function ($user, $index) use($faker, $avatars) {
+                $user->avatar = $faker->randomElement($avatars);
+        });
+
+        // 让隐藏字段可见,并将数据集合转换为数组
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+        User::insert($user_array);
+
+        // 单独处理第一个用户数据
+        $user = User::find(1);
+        $user->name = 'Xiaoxiao';
+        $user->email = 'jixiao35@qq.com';
+        $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->save();
+    }
+}

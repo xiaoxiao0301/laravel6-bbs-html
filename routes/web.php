b0d3f0c26e450d34ddc0774fb4f8c11a07199ca4
xiaoxiao <jixiao35@qq.com>
2019-11-23 15:28:49 +0800

    文件上传

--- routes/web.php
+++ routes/web.php
@@ -36,4 +36,7 @@ Route::resource('users', 'UserController')->only(['show', 'edit', 'update']);
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+

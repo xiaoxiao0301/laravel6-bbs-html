1ccb5298b4335c228d5113cc3e611e009628307f
xiaoxiao <jixiao35@qq.com>
2019-11-23 14:38:00 +0800

    创建话题

--- app/helpers.php
+++ app/helpers.php
@@ -14,3 +14,14 @@ function category_nav_active($category_id)
 //    return active_class(if_route('categories.show') && if_route_param('category', $category_id));
     return active_class(if_route('categories.show') && if_route_param('category', $category_id));
 }
+
+/**
+ * @param $value
+ * @param int $length
+ * @return string
+ */
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r\|\n+/', ' ', $value));
+    return Str::limit($excerpt, $length);
+}
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -23,6 +23,11 @@
         <li class="nav-item"><a class="nav-link" href="{{  route('login') }}">登录</a></li>
         <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
         @else
+          <li class="nav-item">
+            <a href="{{  route('topics.create') }}" class="nav-link mt-1 mr-3 font-weight-bold">
+              <i class="fa fa-plus"></i>
+            </a>
+          </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
               <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
@@ -31,11 +36,18 @@
               {{ Auth::user()->name }}<span class="caret"></span>
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown" role="menu">
-              <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}">个人中心</a>
-              <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}">编辑资料</a>
+              <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}">
+                <i class="far fa-user mr-2"></i>
+                个人中心
+              </a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}">
+                <i class="far fa-edit mr-2"></i>
+                编辑资料
+              </a>
               <div class="dropdown-divider"></div>
               <a class="dropdown-item" id="logout" href="#">
-                <form action="{{ route('logout') }}" method="POST">
+                <form action="{{ route('logout') }}" method="POST" onsubmit="return confirm('您确定要退出吗')">
                   {{ csrf_field() }}
                   <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
                 </form>

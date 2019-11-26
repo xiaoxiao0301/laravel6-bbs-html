368a21e1c0425b21f75b438ac2a4dda619aafa55
xiaoxiao <jixiao35@qq.com>
2019-11-26 15:57:15 +0800

    用户最后活跃时间

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\RecordLastActivedTime;
 use Illuminate\Auth\Middleware\EnsureEmailIsVerified;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
@@ -15,10 +16,15 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        // 修正代理服务器后的服务器参数
         \App\Http\Middleware\TrustProxies::class,
+        // 检测是否应用是否进入『维护模式
         \App\Http\Middleware\CheckForMaintenanceMode::class,
+        // 检测表单请求的数据是否过大
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        // 对提交的请求参数进行 PHP 函数 `trim()` 处理
         \App\Http\Middleware\TrimStrings::class,
+        // 将提交请求参数中空子串转换为 null
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
@@ -29,13 +35,21 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
+            // Cookie 加密解密
             \App\Http\Middleware\EncryptCookies::class,
+            // 将 Cookie 添加到响应中
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            // 开启会话
             \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
+            // 将系统的错误数据注入到视图变量 $errors 中
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            // 检验 CSRF ,防止跨站请求伪造的安全威胁
             \App\Http\Middleware\VerifyCsrfToken::class,
+            // 处理路由绑定
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            // 记录用户最后活跃时间
+            RecordLastActivedTime::class,
         ],
 
         'api' => [
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,15 +3,18 @@
 namespace App\Models;
 
 use Auth;
+use Carbon\Carbon;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Str;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
+    use Traits\LastActivedHelper;
     use Traits\ActiveUserHelper;
     use HasRoles;
     use MustVerifyEmailTrait;
@@ -98,4 +101,6 @@ class User extends Authenticatable implements MustVerifyEmailContract
 
         $this->attributes['avatar'] = $path;
     }
+
+
 }

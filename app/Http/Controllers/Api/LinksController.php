1b4e42b07d7dc9398d4933de431671a50164b723
xiaoxiao <jixiao35@qq.com>
2019-12-17 18:57:05 +0800

    links index

---
+++ app/Http/Controllers/Api/LinksController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Link;
+use App\Transformers\LinkTransformer;
+use Illuminate\Http\Request;
+
+class LinksController extends Controller
+{
+    public function index(Link $link, LinkTransformer $linkTransformer)
+    {
+        $links = $link->getAllCached();
+        return $this->response->collection($links, $linkTransformer);
+    }
+}

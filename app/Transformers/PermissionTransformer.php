6deec16ec96a4ac16bfdf46d2fdeac7f6bf7b496
xiaoxiao <jixiao35@qq.com>
2019-12-17 18:29:18 +0800

    permissions index

---
+++ app/Transformers/PermissionTransformer.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Transformers;
+
+
+use League\Fractal\TransformerAbstract;
+use Spatie\Permission\Models\Permission;
+
+class PermissionTransformer extends TransformerAbstract
+{
+    public function transform(Permission $permission)
+    {
+        return [
+            'id' => $permission->id,
+            'name' => $permission->name,
+        ];
+    }
+
+}
